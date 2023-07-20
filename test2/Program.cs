157ea91d154d0b9c733228dc4614a142a919fe0f
Pavel <pavellast@mail.ru>
2023-07-20 23:18:19 +0300

    done1

--- test2/Program.cs
+++ test2/Program.cs
@@ -8,7 +8,7 @@ int PrintNumbers(int n, int m)
         return 0;
     
     }
-    sum = sum + PrintNumbers(n, m);
+    sum = sum + PrintNumbers(n - 1, m - 1);
     return sum;
 }
 WriteLine("Введите число N");
