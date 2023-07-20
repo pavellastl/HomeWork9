7d9c97186d2b4b59be7ef2b98bf3b6e4124e1d0f
Pavel <pavellast@mail.ru>
2023-07-20 23:05:42 +0300

    done

---
+++ test1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+string PrintNumbers(int n)
+{
+    n = n - (n - 1);
+    if(n == n)
+    {
+        string s = PrintNumbers(n + 1) + ", " + n.ToString();
+    return s;
+    
+    }
+    return n.ToString();
+}
+WriteLine("Введите число N");
+int n = int.Parse(ReadLine()!);
+WriteLine(PrintNumbers(n));
---
+++ test2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+int PrintNumbers(int n, int m)
+{
+    int sum = 0;
+    if(n == m)
+    {
+        return 0;
+    
+    }
+    sum = sum + PrintNumbers(n, m);
+    return sum;
+}
+WriteLine("Введите число N");
+int n = int.Parse(ReadLine());
+WriteLine("Введите число M");
+int m = int.Parse(ReadLine());
+WriteLine(PrintNumbers(n, m));
